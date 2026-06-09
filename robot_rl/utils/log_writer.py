leggedrobotics/rsl_rl

Enable custom logging services (#211)

rsl_rl/utils/log_writer.py
@@ -1,0 +1,43 @@
+# Copyright (c) 2021-2026, ETH Zurich and NVIDIA CORPORATION
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from __future__ import annotations
+
+import pathlib
+from abc import ABC, abstractmethod
+
+
+class LogWriter(ABC):
+    """Abstract base class for logging backends.
+
+    Log writers are configured via ``cfg["logger"]``, a dict with a ``"class_name"`` key pointing to the subclass and
+    any additional keys forwarded as constructor kwargs. The class is resolved via
+    :func:`~rsl_rl.utils.resolve_callable`. Only :meth:`add_scalar` must be implemented; all other methods are no-ops.
+    """
+
+    @abstractmethod
+    def add_scalar(self, tag: str, scalar_value: float, global_step: int) -> None:
+        """Log a scalar metric.
+
+        Args:
+            tag: Name of the metric.
+            scalar_value: Value of the metric.
+            global_step: Current training iteration.
+        """
+
+    def store_config(self, env_cfg: dict | object, train_cfg: dict) -> None:
+        """Upload environment and training configuration. Called once at training start."""
+
+    def save_model(self, model_path: str, it: int) -> None:
+        """Upload a model checkpoint."""
+
+    def save_file(self, path: str) -> None:
+        """Upload an arbitrary file."""
+
+    def save_video(self, video: pathlib.Path, it: int) -> None:
+        """Upload a video file."""
+
+    def stop(self) -> None:
+        """Finalize and close the logging run."""

rsl_rl/utils/logger.py
@@ -12,9 +12,12 @@ import pathlib
 import statistics
 import time
 import torch
+import warnings
 from collections import deque
 
 import rsl_rl
+from rsl_rl.utils.log_writer import LogWriter
+from rsl_rl.utils.utils import resolve_callable
 
 
 class Logger:
@@ -42,6 +45,9 @@ class Logger:
         self.tot_timesteps = 0
         self.tot_time = 0
 
+        self.writer: LogWriter | None = None
+        self.logger_type: str | None = None
+
         # Create buffers
         self.ep_extras = []
         self.rewbuffer = deque(maxlen=100)
@@ -61,38 +67,56 @@ class Logger:
         self.disable_logs = is_distributed and gpu_global_rank != 0
 
     def init_logging_writer(self) -> None:
-        """Initialize the logging writer, which can be either Tensorboard, W&B or Neptune and save the code state.
+        """Initialize the logging writer and save the code state.
 
-        If the writer is either W&B or Neptune, the configuration and code state are uploaded as well.
+        .. note::
+            The writer is constructed from ``cfg["logger"]``, which should be a dict with a ``"class_name"`` key plus
+            any additional constructor kwargs (see :class:`~rsl_rl.utils.LogWriter`). The plain string aliases
+            ``"wandb"`` and ``"neptune"`` are deprecated; use ``"WandbLogWriter"`` and ``"NeptuneLogWriter"`` in the
+            dict form instead. ``"tensorboard"`` (the default) is still accepted as a plain string.
         """
         if self.log_dir is not None and not self.disable_logs:
-            self.logger_type = self.cfg.get("logger", "tensorboard")
-            self.logger_type = self.logger_type.lower()
-            if self.logger_type == "neptune":
-                from rsl_rl.utils.neptune_utils import NeptuneSummaryWriter
+            logger_cfg = self.cfg.get("logger", "tensorboard")
+            self.logger_type = logger_cfg if isinstance(logger_cfg, str) else logger_cfg.pop("class_name")
 
-                self.writer = NeptuneSummaryWriter(log_dir=self.log_dir, flush_secs=10, cfg=self.cfg)
-            elif self.logger_type == "wandb":
-                from rsl_rl.utils.wandb_utils import WandbSummaryWriter
+            # Handle deprecated plain string logger types for W&B and Neptune
+            if self.logger_type == "wandb" and isinstance(logger_cfg, str):
+                warnings.warn(
+                    "cfg['logger'] = 'wandb' is deprecated. "
+                    "Use cfg['logger'] = {'class_name': 'WandbLogWriter', 'project_name': ...} instead.",
+                    DeprecationWarning,
+                    stacklevel=2,
+                )
+                self.logger_type = "WandbLogWriter"
+                logger_cfg = {"project_name": self.cfg.get("wandb_project")}
+            elif self.logger_type == "neptune" and isinstance(logger_cfg, str):
+                warnings.warn(
+                    "cfg['logger'] = 'neptune' is deprecated. "
+                    "Use cfg['logger'] = {'class_name': 'NeptuneLogWriter', 'project_name': ...} instead.",
+                    DeprecationWarning,
+                    stacklevel=2,
+                )
+                self.logger_type = "NeptuneLogWriter"
+                logger_cfg = {"project_name": self.cfg.get("neptune_project")}
 
-                self.writer = WandbSummaryWriter(log_dir=self.log_dir, flush_secs=10, cfg=self.cfg)
-            elif self.logger_type == "tensorboard":
+            if self.logger_type == "tensorboard":
                 from torch.utils.tensorboard import SummaryWriter
 
-                self.writer = SummaryWriter(log_dir=self.log_dir, flush_secs=10)
+                self.writer = SummaryWriter(log_dir=self.log_dir, flush_secs=10)  # type: ignore
             else:
-                raise ValueError("Logger type not found. Please choose 'wandb', 'neptune', or 'tensorboard'.")
+                writer_class = resolve_callable(self.logger_type)
+                self.writer = writer_class(log_dir=self.log_dir, **logger_cfg)  # type: ignore
         else:
             self.writer = None
 
         # Save code state
         files_to_upload = self._store_code_state()
 
-        # Upload configuration and code state to external logging service if applicable
-        if self.writer is not None and self.logger_type in ["wandb", "neptune"]:
-            self.writer.store_config(self.env_cfg, self.cfg)  # type: ignore
+        # Upload configuration and code state to external logging service if supported
+        if isinstance(self.writer, LogWriter):
+            self.writer.store_config(self.env_cfg, self.cfg)
             for path in files_to_upload:
-                self.writer.save_file(path)  # type: ignore
+                self.writer.save_file(path)
 
     def process_env_step(
         self,
@@ -200,7 +224,7 @@ class Logger:
                     self.writer.add_scalar("Rnd/weight", rnd_weight, it)  # type: ignore
                 self.writer.add_scalar("Train/mean_reward", statistics.mean(self.rewbuffer), it)
                 self.writer.add_scalar("Train/mean_episode_length", statistics.mean(self.lenbuffer), it)
-                if self.logger_type != "wandb":
+                if self.logger_type != "WandbLogWriter":
                     self.writer.add_scalar(
                         "Train/mean_reward/time", statistics.mean(self.rewbuffer), int(self.tot_time)
                     )
@@ -255,23 +279,23 @@ class Logger:
             )
             print(log_string)
 
-            # Upload available videos
-            if self.logger_type == "wandb":
+            # Upload available videos to external logging service if supported
+            if isinstance(self.writer, LogWriter):
                 for video in pathlib.Path(self.log_dir).rglob("*.mp4"):  # type: ignore
-                    self.writer.save_video(video, it)  # type: ignore
+                    self.writer.save_video(video, it)
 
             # Clear extras buffer
             self.ep_extras.clear()
 
     def save_model(self, path: str, it: int) -> None:
-        """Save the model to external logging services if specified."""
-        if self.writer is not None and self.logger_type in ["neptune", "wandb"]:
-            self.writer.save_model(path, it)  # type: ignore
+        """Save the model to external logging service if specified."""
+        if isinstance(self.writer, LogWriter):
+            self.writer.save_model(path, it)
 
     def stop_logging_writer(self) -> None:
         """Stop the logging writer."""
-        if self.writer is not None and self.logger_type in ["neptune", "wandb"]:
-            self.writer.stop()  # type: ignore
+        if isinstance(self.writer, LogWriter):
+            self.writer.stop()
 
     def _store_code_state(self) -> list[str]:
         """Store the current git diff of the code repositories involved in the experiment."""

rsl_rl/utils/neptune_log_writer.py
@@ -10,27 +10,26 @@ import os
 from dataclasses import asdict
 from torch.utils.tensorboard import SummaryWriter
 
+from rsl_rl.utils.log_writer import LogWriter
+
 try:
     import neptune
 except ModuleNotFoundError:
-    raise ModuleNotFoundError("neptune-client is required to log to Neptune.") from None
+    neptune = None
 
 
-class NeptuneSummaryWriter(SummaryWriter):
+class NeptuneLogWriter(SummaryWriter, LogWriter):
     """Summary writer for Neptune."""
 
-    def __init__(self, log_dir: str, flush_secs: int, cfg: dict) -> None:
+    def __init__(self, log_dir: str, project_name: str) -> None:
         """Initialize a Neptune run for logging."""
-        super().__init__(log_dir, flush_secs=flush_secs)
+        if neptune is None:
+            raise ModuleNotFoundError("neptune-client is required to log to Neptune.")
+        super().__init__(log_dir, flush_secs=10)
 
         # Get the run name
         run_name = os.path.split(log_dir)[-1]
 
-        # Get neptune project and entity
-        try:
-            project = cfg["neptune_project"]
-        except KeyError:
-            raise KeyError("Please specify neptune_project in the runner config, e.g. legged_gym.") from None
         try:
             token = os.environ["NEPTUNE_API_TOKEN"]
         except KeyError:
@@ -45,7 +44,7 @@ class NeptuneSummaryWriter(SummaryWriter):
             ) from None
 
         # Initialize neptune
-        neptune_project = entity + "/" + project
+        neptune_project = entity + "/" + project_name
         self.run = neptune.init_run(project=neptune_project, api_token=token)
         self.run["log_dir"].log(run_name)
 
@@ -54,14 +53,6 @@ class NeptuneSummaryWriter(SummaryWriter):
             "Train/mean_reward/time": "Train/mean_reward_time",
             "Train/mean_episode_length/time": "Train/mean_episode_length_time",
         }
-
-    def store_config(self, env_cfg: dict | object, train_cfg: dict) -> None:
-        """Upload environment and training configuration to Neptune."""
-        self.run["train_cfg"] = train_cfg
-        try:
-            self.run["env_cfg"] = env_cfg.to_dict()  # type: ignore
-        except Exception:
-            self.run["env_cfg"] = asdict(env_cfg)  # type: ignore
 
     def add_scalar(
         self,
@@ -72,18 +63,16 @@ class NeptuneSummaryWriter(SummaryWriter):
         new_style: bool = False,
     ) -> None:
         """Log a scalar to both TensorBoard and Neptune."""
-        super().add_scalar(
-            tag,
-            scalar_value,
-            global_step=global_step,
-            walltime=walltime,
-            new_style=new_style,
-        )
+        super().add_scalar(tag, scalar_value, global_step=global_step, walltime=walltime, new_style=new_style)
         self.run[self._map_path(tag)].log(scalar_value, step=global_step)
 
-    def stop(self) -> None:
-        """Finish the active Neptune run."""
-        self.run.stop()
+    def store_config(self, env_cfg: dict | object, train_cfg: dict) -> None:
+        """Upload environment and training configuration to Neptune."""
+        self.run["train_cfg"] = train_cfg
+        try:
+            self.run["env_cfg"] = env_cfg.to_dict()  # type: ignore
+        except Exception:
+            self.run["env_cfg"] = asdict(env_cfg)  # type: ignore
 
     def save_model(self, model_path: str, it: int) -> None:
         """Upload a model checkpoint artifact to Neptune."""
@@ -93,6 +82,10 @@ class NeptuneSummaryWriter(SummaryWriter):
         """Upload an arbitrary file artifact to Neptune."""
         name = path.rsplit("/", 1)[-1].split(".")[0]
         self.run["git_diff/" + name].upload(path)
+
+    def stop(self) -> None:
+        """Finish the active Neptune run."""
+        self.run.stop()
 
     def _map_path(self, path: str) -> str:
         """Map metric names to Neptune-compatible keys."""

Creampelt/robot_rl

robot_rl/utils/log_writer.py
@@ -1,0 +1,43 @@
+# Copyright (c) 2021-2026, ETH Zurich and NVIDIA CORPORATION
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+from __future__ import annotations
+
+import pathlib
+from abc import ABC, abstractmethod
+
+
+class LogWriter(ABC):
+    """Abstract base class for logging backends.
+
+    Log writers are configured via ``cfg["logger"]``, a dict with a ``"class_name"`` key pointing to the subclass and
+    any additional keys forwarded as constructor kwargs. The class is resolved via
+    :func:`~robot_rl.utils.resolve_callable`. Only :meth:`add_scalar` must be implemented; all other methods are no-ops.
+    """
+
+    @abstractmethod
+    def add_scalar(self, tag: str, scalar_value: float, global_step: int) -> None:
+        """Log a scalar metric.
+
+        Args:
+            tag: Name of the metric.
+            scalar_value: Value of the metric.
+            global_step: Current training iteration.
+        """
+
+    def store_config(self, env_cfg: dict | object, train_cfg: dict) -> None:
+        """Upload environment and training configuration. Called once at training start."""
+
+    def save_model(self, model_path: str, it: int) -> None:
+        """Upload a model checkpoint."""
+
+    def save_file(self, path: str) -> None:
+        """Upload an arbitrary file."""
+
+    def save_video(self, video: pathlib.Path, it: int) -> None:
+        """Upload a video file."""
+
+    def stop(self) -> None:
+        """Finalize and close the logging run."""

robot_rl/utils/logger.py
@@ -12,9 +12,12 @@ import pathlib
 import statistics
 import time
 import torch
+import warnings
 from collections import deque
 
 import robot_rl
+from robot_rl.utils.log_writer import LogWriter
+from robot_rl.utils.utils import resolve_callable
 
 
 class Logger:
@@ -42,6 +45,9 @@ class Logger:
         self.tot_timesteps = 0
         self.tot_time = 0
 
+        self.writer: LogWriter | None = None
+        self.logger_type: str | None = None
+
         # Create buffers
         self.ep_extras = []
         self.rewbuffer = deque(maxlen=100)
@@ -61,40 +67,67 @@ class Logger:
         self.disable_logs = is_distributed and gpu_global_rank != 0
 
     def init_logging_writer(self) -> None:
-        """Initialize the logging writer, which can be either Tensorboard, W&B or Neptune and save the code state.
+        """Initialize the logging writer and save the code state.
 
-        If the writer is either W&B or Neptune, the configuration and code state are uploaded as well.
+        .. note::
+            The writer is constructed from ``cfg["logger"]``, which should be a dict with a ``"class_name"`` key plus
+            any additional constructor kwargs (see :class:`~robot_rl.utils.LogWriter`). The plain string aliases
+            ``"wandb"`` and ``"neptune"`` are deprecated; use ``"WandbLogWriter"`` and ``"NeptuneLogWriter"`` in the
+            dict form instead. ``"tensorboard"`` (the default) is still accepted as a plain string.
         """
         if self.log_dir is not None and not self.disable_logs:
-            self.logger_type = self.cfg.get("logger", "tensorboard")
-            self.logger_type = self.logger_type.lower()
-            if self.logger_type == "neptune":
-                from robot_rl.utils.neptune_utils import NeptuneSummaryWriter
+            logger_cfg = self.cfg.get("logger", "tensorboard")
+            self.logger_type = logger_cfg if isinstance(logger_cfg, str) else logger_cfg.pop("class_name")
 
-                self.writer = NeptuneSummaryWriter(log_dir=self.log_dir, flush_secs=10, cfg=self.cfg)
-            elif self.logger_type == "wandb":
-                from robot_rl.utils.wandb_utils import WandbSummaryWriter
-
-                self.writer = WandbSummaryWriter(
-                    log_dir=self.log_dir, flush_secs=10, num_envs=self.num_envs, cfg=self.cfg
+            # Handle deprecated plain string logger types for W&B and Neptune
+            if self.logger_type == "wandb" and isinstance(logger_cfg, str):
+                warnings.warn(
+                    "cfg['logger'] = 'wandb' is deprecated. "
+                    "Use cfg['logger'] = {'class_name': 'WandbLogWriter', 'project_name': ...} instead.",
+                    DeprecationWarning,
+                    stacklevel=2,
                 )
-            elif self.logger_type == "tensorboard":
+                self.logger_type = "WandbLogWriter"
+                logger_cfg = {
+                    "project_name": self.cfg.get("wandb_project"),
+                    "run_name": self.cfg.get("wandb_run_name"),
+                    "group": self.cfg.get("wandb_group"),
+                    "shared": self.cfg.get("shared", False),
+                    "log_videos_async": self.cfg.get("log_videos_async", False),
+                }
+            elif self.logger_type == "neptune" and isinstance(logger_cfg, str):
+                warnings.warn(
+                    "cfg['logger'] = 'neptune' is deprecated. "
+                    "Use cfg['logger'] = {'class_name': 'NeptuneLogWriter', 'project_name': ...} instead.",
+                    DeprecationWarning,
+                    stacklevel=2,
+                )
+                self.logger_type = "NeptuneLogWriter"
+                logger_cfg = {"project_name": self.cfg.get("neptune_project")}
+
+            if self.logger_type == "tensorboard":
                 from torch.utils.tensorboard import SummaryWriter
 
-                self.writer = SummaryWriter(log_dir=self.log_dir, flush_secs=10)
+                self.writer = SummaryWriter(log_dir=self.log_dir, flush_secs=10)  # type: ignore
             else:
-                raise ValueError("Logger type not found. Please choose 'wandb', 'neptune', or 'tensorboard'.")
+                from robot_rl.utils.wandb_log_writer import WandbLogWriter
+
+                writer_class = resolve_callable(self.logger_type)
+                # The W&B writer logs environment steps as a custom metric, so inject the env count
+                if isinstance(writer_class, type) and issubclass(writer_class, WandbLogWriter):
+                    logger_cfg.setdefault("num_envs", self.num_envs)
+                self.writer = writer_class(log_dir=self.log_dir, **logger_cfg)  # type: ignore
         else:
             self.writer = None
 
         # Save code state
         files_to_upload = self._store_code_state()
 
-        # Upload configuration and code state to external logging service if applicable
-        if self.writer is not None and self.logger_type in ["wandb", "neptune"]:
-            self.writer.store_config(self.env_cfg, self.cfg)  # type: ignore
+        # Upload configuration and code state to external logging service if supported
+        if isinstance(self.writer, LogWriter):
+            self.writer.store_config(self.env_cfg, self.cfg)
             for path in files_to_upload:
-                self.writer.save_file(path)  # type: ignore
+                self.writer.save_file(path)
 
     def process_env_step(
         self,
@@ -202,7 +235,7 @@ class Logger:
                     self.writer.add_scalar("Rnd/weight", rnd_weight, it)  # type: ignore
                 self.writer.add_scalar("Train/mean_reward", statistics.mean(self.rewbuffer), it)
                 self.writer.add_scalar("Train/mean_episode_length", statistics.mean(self.lenbuffer), it)
-                if self.logger_type != "wandb":
+                if self.logger_type != "WandbLogWriter":
                     self.writer.add_scalar(
                         "Train/mean_reward/time", statistics.mean(self.rewbuffer), int(self.tot_time)
                     )
@@ -257,27 +290,28 @@ class Logger:
             )
             print(log_string)
 
-            # Upload available videos. Skip the out-of-process video logger's own dir: it attaches
-            # to this same W&B run and logs those clips at their checkpoint step, so re-uploading
-            # here would duplicate each video at the (drifted) current iteration.
-            if self.logger_type == "wandb":
+            # Upload available videos to external logging service if supported. Skip the out-of-process
+            # video logger's own dir: it attaches to this same W&B run and logs those clips at their
+            # checkpoint step, so re-uploading here would duplicate each video at the (drifted) current
+            # iteration.
+            if isinstance(self.writer, LogWriter):
                 for video in pathlib.Path(self.log_dir).rglob("*.mp4"):  # type: ignore
                     if "video_logger" in video.parts:
                         continue
-                    self.writer.save_video(video, it)  # type: ignore
+                    self.writer.save_video(video, it)
 
             # Clear extras buffer
             self.ep_extras.clear()
 
     def save_model(self, path: str, it: int) -> None:
-        """Save the model to external logging services if specified."""
-        if self.writer is not None and self.logger_type in ["neptune", "wandb"]:
-            self.writer.save_model(path, it)  # type: ignore
+        """Save the model to external logging service if specified."""
+        if isinstance(self.writer, LogWriter):
+            self.writer.save_model(path, it)
 
     def stop_logging_writer(self) -> None:
         """Stop the logging writer."""
-        if self.writer is not None and self.logger_type in ["neptune", "wandb"]:
-            self.writer.stop()  # type: ignore
+        if isinstance(self.writer, LogWriter):
+            self.writer.stop()
 
     def _store_code_state(self) -> list[str]:
         """Store the current git diff of the code repositories involved in the experiment."""

robot_rl/utils/neptune_log_writer.py
@@ -10,27 +10,26 @@ import os
 from dataclasses import asdict
 from torch.utils.tensorboard import SummaryWriter
 
+from robot_rl.utils.log_writer import LogWriter
+
 try:
     import neptune
 except ModuleNotFoundError:
-    raise ModuleNotFoundError("neptune-client is required to log to Neptune.") from None
+    neptune = None
 
 
-class NeptuneSummaryWriter(SummaryWriter):
+class NeptuneLogWriter(SummaryWriter, LogWriter):
     """Summary writer for Neptune."""
 
-    def __init__(self, log_dir: str, flush_secs: int, cfg: dict) -> None:
+    def __init__(self, log_dir: str, project_name: str) -> None:
         """Initialize a Neptune run for logging."""
-        super().__init__(log_dir, flush_secs=flush_secs)
+        if neptune is None:
+            raise ModuleNotFoundError("neptune-client is required to log to Neptune.")
+        super().__init__(log_dir, flush_secs=10)
 
         # Get the run name
         run_name = os.path.split(log_dir)[-1]
 
-        # Get neptune project and entity
-        try:
-            project = cfg["neptune_project"]
-        except KeyError:
-            raise KeyError("Please specify neptune_project in the runner config, e.g. legged_gym.") from None
         try:
             token = os.environ["NEPTUNE_API_TOKEN"]
         except KeyError:
@@ -45,7 +44,7 @@ class NeptuneSummaryWriter(SummaryWriter):
             ) from None
 
         # Initialize neptune
-        neptune_project = entity + "/" + project
+        neptune_project = entity + "/" + project_name
         self.run = neptune.init_run(project=neptune_project, api_token=token)
         self.run["log_dir"].log(run_name)
 
@@ -54,14 +53,6 @@ class NeptuneSummaryWriter(SummaryWriter):
             "Train/mean_reward/time": "Train/mean_reward_time",
             "Train/mean_episode_length/time": "Train/mean_episode_length_time",
         }
-
-    def store_config(self, env_cfg: dict | object, train_cfg: dict) -> None:
-        """Upload environment and training configuration to Neptune."""
-        self.run["train_cfg"] = train_cfg
-        try:
-            self.run["env_cfg"] = env_cfg.to_dict()  # type: ignore
-        except Exception:
-            self.run["env_cfg"] = asdict(env_cfg)  # type: ignore
 
     def add_scalar(
         self,
@@ -72,18 +63,16 @@ class NeptuneSummaryWriter(SummaryWriter):
         new_style: bool = False,
     ) -> None:
         """Log a scalar to both TensorBoard and Neptune."""
-        super().add_scalar(
-            tag,
-            scalar_value,
-            global_step=global_step,
-            walltime=walltime,
-            new_style=new_style,
-        )
+        super().add_scalar(tag, scalar_value, global_step=global_step, walltime=walltime, new_style=new_style)
         self.run[self._map_path(tag)].log(scalar_value, step=global_step)
 
-    def stop(self) -> None:
-        """Finish the active Neptune run."""
-        self.run.stop()
+    def store_config(self, env_cfg: dict | object, train_cfg: dict) -> None:
+        """Upload environment and training configuration to Neptune."""
+        self.run["train_cfg"] = train_cfg
+        try:
+            self.run["env_cfg"] = env_cfg.to_dict()  # type: ignore
+        except Exception:
+            self.run["env_cfg"] = asdict(env_cfg)  # type: ignore
 
     def save_model(self, model_path: str, it: int) -> None:
         """Upload a model checkpoint artifact to Neptune."""
@@ -93,6 +82,10 @@ class NeptuneSummaryWriter(SummaryWriter):
         """Upload an arbitrary file artifact to Neptune."""
         name = path.rsplit("/", 1)[-1].split(".")[0]
         self.run["git_diff/" + name].upload(path)
+
+    def stop(self) -> None:
+        """Finish the active Neptune run."""
+        self.run.stop()
 
     def _map_path(self, path: str) -> str:
         """Map metric names to Neptune-compatible keys."""